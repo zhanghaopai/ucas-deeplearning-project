031053013c617cb0902fdd1118f044b95a802b36
zhang_haopai <zhang_haopai@venusgroup.com.cn>
2024-04-11 10:22:41 +0800

    提交代码，不提交数据

---
+++ cifar10.py
@@ -0,0 +1,30 @@
+import torch
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
+
+# 超参数定义
+BATCH_SIZE = 512  # batch size
+EPOCHS = 20  # 训练批次
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# 定义训练集和测试集
+train_datasets = datasets.CIFAR10(
+    root="data", transform=transforms.ToTensor(), train=True, download=True
+)
+test_datasets = datasets.CIFAR10(
+    root="data", transform=transforms.ToTensor(), train=False, download=True
+)
+# 定义dataloader
+train_dataloader = DataLoader(train_datasets, batch_size=BATCH_SIZE, shuffle=True)
+test_dataloader = DataLoader(test_datasets, batch_size=BATCH_SIZE, shuffle=False)
+
+
+train_feature, train_label = next(iter(train_dataloader))
+img = train_feature[0].squeeze()
+img = img.permute(1, 2, 0)
+print(img.shape)
+label = train_label[0]
+plt.imshow(img)
+plt.show()
