2a1bd1e48b4e86a3f76ad3f31f9cb458866b5625
zhang_haopai <zhang_haopai@venusgroup.com.cn>
2024-04-15 09:16:06 +0800

    提交adam优化器

--- cifar10.py
+++ cifar10.py
@@ -13,13 +13,10 @@ ssl._create_default_https_context = ssl._create_unverified_context
 # 超参数定义
 BATCH_SIZE = 512  # batch size
 EPOCHS = 20  # 训练批次
-DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 LEARNING_RATE = 0.01
 MOMENTUM = 0.9
-
-# 获取device
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # 定义训练集和测试集
 train_datasets = datasets.CIFAR10(
@@ -53,8 +50,9 @@ if __name__ == '__main__':
     # 模型
     model = MLP(input_size=input_size, classes_num=classes_num)
     # 优化器
-    optimizer = torch.optim.SGD(model.parameters(), LEARNING_RATE, MOMENTUM)
+    SGD_optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
+    adam_optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
     # 训练
-    train(device=device, epochs=EPOCHS, train_loader=train_dataloader,
+    train(device=DEVICE, epochs=EPOCHS, train_loader=train_dataloader,
           test_loader=test_dataloader, input_size=input_size, model=model,
-          optimizer=optimizer, loss_function=F.cross_entropy)
+          optimizer=adam_optimizer, loss_function=F.cross_entropy)
