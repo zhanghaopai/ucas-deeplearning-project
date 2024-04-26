e659bbda82304b53b4e63033d9c0d2aa5ee5fb5e
zhang_haopai <zhang_haopai@venusgroup.com.cn>
2024-04-26 09:25:55 +0800

    恢复resnet

--- models/ResNet.py
+++ models/ResNet.py
@@ -21,10 +21,10 @@ class BasicBlock(nn.Module):
             )
 
     def forward(self, x):
-        out = torch.relu(self.bn1(self.conv1(x)))
+        out = F.relu(self.bn1(self.conv1(x)))
         out = self.bn2(self.conv2(out))
         out += self.shortcut(x)
-        out = torch.relu(out)
+        out = F.relu(out)
         return out
 
 class ResNet(nn.Module):
